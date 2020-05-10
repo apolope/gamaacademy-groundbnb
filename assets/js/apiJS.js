2d951f702cd6d920827a49a241fe867f96308c73
apolope <apolonio.serafim@hotmail.com>
2020-05-10 09:26:56 -0300

    Alteração para teste

--- assets/js/apiJS.js
+++ assets/js/apiJS.js
@@ -113,3 +113,13 @@ $(document).ready(function() {
         }
     });
 });
+
+//Teste
+let parts = window.location.search.substr(1).split("&");
+let $_GET = {};
+for (let i = 0; i < parts.length; i++) {
+    let temp = parts[i].split("=");
+    $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
+}
+
+alert($_GET.pag);    // 2
